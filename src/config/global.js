5eea522eee4db719eeefa3d44613b3ab1932e709
Sebastian Trujillo <129899570+strujilloa@users.noreply.github.com>
2023-07-05 07:40:54 -0500

    update CFA

--- src/config/global.js
+++ src/config/global.js
@@ -171,6 +171,11 @@ export default {
         download: 'downloads/material.zip',
       },
       {
+        icono: 'fas fa-download',
+        titulo: 'Descargar PDF',
+        download: 'downloads/CFA_04_821207.pdf',
+      },
+      {
         icono: 'far fa-registered',
         titulo: 'Créditos',
         nombreRuta: 'creditos',
